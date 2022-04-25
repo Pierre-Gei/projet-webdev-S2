3e0e3726c6d0e7530b00e4cdfff87c6148194e30
Pierre-Gei <102214308+Pierre-Gei@users.noreply.github.com>
2022-04-25 08:23:40 +0200

    v1.2.1
    dés corrigés

--- lancer-des-dés/ftest.js
+++ lancer-des-dés/ftest.js
@@ -7,43 +7,43 @@ function roll(){
     let x1 = Math.floor((Math.random()*6)+1);
     switch(x1){
         case 1:
-            output1.src = "/557px-Dice-1-b.svg.png";
+            output1.src = "557px-Dice-1-b.svg.png";
             break
         case 2:
-            output1.src = "/557px-Dice-2-b.svg.png";
+            output1.src = "557px-Dice-2-b.svg.png";
             break
         case 3:
-            output1.src = "/557px-Dice-3-b.svg.png";
+            output1.src = "557px-Dice-3-b.svg.png";
             break
         case 4:
-            output1.src = "/Dice-4-b.svg.png";
+            output1.src = "Dice-4-b.svg.png";
             break
         case 5:
-            output1.src = "/Dice-5-b.svg.png";
+            output1.src = "Dice-5-b.svg.png";
             break
         case 6:
-            output1.src = "/Dice-6-b.svg.png";
+            output1.src = "Dice-6-b.svg.png";
             break;            
     }  
     let x2 = Math.floor((Math.random()*6)+1);
     switch(x2){
         case 1:
-            output2.src = "/557px-Dice-1-b.svg.png";
+            output2.src = "557px-Dice-1-b.svg.png";
             break
         case 2:
-            output2.src = "/557px-Dice-2-b.svg.png";
+            output2.src = "557px-Dice-2-b.svg.png";
             break
         case 3:
-            output2.src = "/557px-Dice-3-b.svg.png";
+            output2.src = "557px-Dice-3-b.svg.png";
             break
         case 4:
-            output2.src = "/Dice-4-b.svg.png";
+            output2.src = "Dice-4-b.svg.png";
             break
         case 5:
-            output2.src = "/Dice-5-b.svg.png";
+            output2.src = "Dice-5-b.svg.png";
             break
         case 6:
-            output2.src = "/Dice-6-b.svg.png";
+            output2.src = "Dice-6-b.svg.png";
             break;            
     }
     output3.innerHTML = x1 + x2;
